787c92fca6f5b79896b6ea2865653f08df188606
annead0 <120595101+annead0@users.noreply.github.com>
2023-06-24 19:17:15 +0100

    Create send_email.php

---
+++ ibcv/send_email.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $to = "annead.ibcv@gmail.com";
+  $subject = "Contactar a Annead";
+
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+
+  $headers = "From: $email\r\n";
+  $headers .= "Reply-To: $email\r\n";
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+  // Send the email
+  $mail_sent = mail($to, $subject, $message, $headers);
+
+  if ($mail_sent) {
+    echo "Enviat sense problemes!";
+  } else {
+    echo "A fallat.";
+  }
+}
+?>
