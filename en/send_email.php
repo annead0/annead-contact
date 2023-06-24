829ed46fe5ac7450e898af500fdbe36b49151380
annead0 <120595101+annead0@users.noreply.github.com>
2023-06-24 19:06:06 +0100

    Create send_email.php

---
+++ en/send_email.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $to = "recipient@example.com";
+  $subject = "Contact Form Submission";
+
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+
+  $headers = "From: $email\r\n";
+  $headers .= "Reply-To: $email\r\n";
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+  // Send the email
+  $mail_sent = mail($to, $subject, $message, $headers);
+
+  if ($mail_sent) {
+    echo "Email sent successfully.";
+  } else {
+    echo "Failed to send email.";
+  }
+}
+?>
